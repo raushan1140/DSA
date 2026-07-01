45a31651c54da7de91e457b3ee96b31c65429686
Raushan Raj <raj.raushan9101@gmail.com>
2026-07-01 18:11:06 +0530

    Time: 243 ms (12.06%) | Memory: 117.5 MB (74.29%) - LeetSync

---
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
@@ -0,0 +1,133 @@
+class Solution {
+    int dir[][] = {{1,0},{-1,0},{0,1},{0,-1}};
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+        int n = grid.size();
+
+        int dist[][] = new int[n][n];
+
+        for(int i = 0; i < n; i++){
+
+            Arrays.fill(dist[i], -1);
+        }
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        // Multi-source BFS from all thieves
+        for(int i = 0; i < n; i++){
+
+            for(int j = 0; j < n; j++){
+
+                if(grid.get(i).get(j) == 1){
+
+                    dist[i][j] = 0;
+
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        while(!queue.isEmpty()){
+
+            int cur[] = queue.poll();
+
+            int x = cur[0];
+            int y = cur[1];
+
+            for(int d[] : dir){
+
+                int nx = x + d[0];
+                int ny = y + d[1];
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >= n){
+
+                    continue;
+                }
+
+                if(dist[nx][ny] != -1){
+
+                    continue;
+                }
+
+                dist[nx][ny] = dist[x][y] + 1;
+
+                queue.offer(new int[]{nx, ny});
+            }
+        }
+
+        int low = 0;
+        int high = 2 * n;
+
+        while(low <= high){
+
+            int mid = low + (high - low) / 2;
+
+            if(canReach(dist, mid)){
+
+                low = mid + 1;
+            }
+            else{
+
+                high = mid - 1;
+            }
+        }
+
+        return high;
+    }
+
+    private boolean canReach(int dist[][], int safe){
+
+        int n = dist.length;
+
+        if(dist[0][0] < safe){
+
+            return false;
+        }
+
+        boolean vis[][] = new boolean[n][n];
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.offer(new int[]{0, 0});
+
+        vis[0][0] = true;
+
+        while(!queue.isEmpty()){
+
+            int cur[] = queue.poll();
+
+            int x = cur[0];
+            int y = cur[1];
+
+            if(x == n - 1 && y == n - 1){
+
+                return true;
+            }
+
+            for(int d[] : dir){
+
+                int nx = x + d[0];
+                int ny = y + d[1];
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >= n){
+
+                    continue;
+                }
+
+                if(vis[nx][ny]){
+
+                    continue;
+                }
+
+                if(dist[nx][ny] < safe){
+
+                    continue;
+                }
+
+                vis[nx][ny] = true;
+
+                queue.offer(new int[]{nx, ny});
+            }
+        }
+        return false;
+    }
+}
